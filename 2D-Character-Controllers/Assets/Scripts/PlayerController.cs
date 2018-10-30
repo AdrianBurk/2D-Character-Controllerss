4fe6abccbc4dd0fd07dad1e7b6a603c98df4b8bb
Adrian Burk <adrian.burk@loop.colum.edu>
2018-10-30 15:10:50 -0500

    Started Controller Script

--- 2D-Character-Controllers/Assets/Scripts/PlayerController.cs
+++ 2D-Character-Controllers/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
 
 	// Use this for initialization
 	void Start () {
