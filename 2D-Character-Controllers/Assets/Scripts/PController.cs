e70c52489e2fe07e10c286441f99cd170b291296
Adrian Burk <adrian.burk@loop.colum.edu>
2018-11-13 14:31:56 -0600

    Finished adding Jump plus animations
    I get 2 Null Reference Exceptions even after following all the instructions and have no clue how to fix it

---
+++ 2D-Character-Controllers/Assets/Scripts/PController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PController : MonoBehaviour
+{
+
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+    Animator anim;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 700f;
+    bool doubleJump = false;
+    private Rigidbody2D rb2d;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        if (grounded)
+            doubleJump = false;
+
+        anim.SetFloat("vSpeed", rb2d.velocity.y);
+
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+
+    }
+
+    void Update()
+    {
+        if(grounded || !doubleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rb2d.AddForce(new Vector2(0, jumpForce));
+
+            if (!doubleJump && !grounded)
+                doubleJump = true;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
